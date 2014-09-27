080ebee0d2f927901bfea1851a3177563e323bcd
AndersonMAaron <AndersonMAaron@gmail.com>
2014-09-27 14:59:22 -0400

    Updated obstacle

--- Disastroid/src/com/aj/games/disastroid/obstacle/Obstacle.java
+++ Disastroid/src/com/aj/games/disastroid/obstacle/Obstacle.java
@@ -1,5 +1,7 @@
 package com.aj.games.disastroid.obstacle;
 
+import android.graphics.Point;
+
 import com.aj.games.disastroid.ship.Ship;
 
 /**
@@ -10,21 +12,40 @@ import com.aj.games.disastroid.ship.Ship;
  * If the ship's wing's center is within that zone, it is considered a hit. 
  */
 public class Obstacle {
-	private int x;
-	private int y; 
+	private Point location;
 	private int diameter;
 	private int zoomPct;
+	private int zoomSpeed;
 	
-	public Obstacle(int x, int y, int diameter) {
-		this.x = x;
-		this.y = y;
+	public final int DEFAULT_ZOOM_SPD = 1;
+	
+	public Obstacle(Point location, int diameter) {
+		this.location = location;
 		this.diameter = diameter;
+		zoomSpeed = DEFAULT_ZOOM_SPD;
 		zoomPct = 0;
 	}
+	
+	public void onUpdate() {
+		zoomPct += zoomSpeed;
+	}
+	
+	public boolean hasPassed() { return (zoomPct >= 100); }
 
 	public boolean isHittingShip(Ship ship) {
 		if (zoomPct < 90) { return false; }
 		return false;
 	}
 	
+	/* GETTERS */
+	public int getX() { return location.x; }
+	public int getY() { return location.y; }
+	public Point getLocation() { return location; }
+	public int getDiameter() { return diameter; }
+	public int getZoomPct() { return zoomPct; }
+
+	/* SETTERS */
+	public void setLocation(Point location) { this.location = location; }
+	public void setDiameter(int diameter) { this.diameter = diameter; }
+	public void setZoomPct(int zoomPct) { this.zoomPct = zoomPct; }
 }
