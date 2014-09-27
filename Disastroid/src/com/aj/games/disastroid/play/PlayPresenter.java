5740da13df6e239e4234927599202160488adebd
gjinfo <gjinfo737@gmail.com>
2014-09-27 15:24:14 -0400

    wip populater

--- Disastroid/src/com/aj/games/disastroid/play/PlayPresenter.java
+++ Disastroid/src/com/aj/games/disastroid/play/PlayPresenter.java
@@ -1,18 +1,23 @@
 package com.aj.games.disastroid.play;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.Display;
 import android.widget.Toast;
 
 import com.aj.games.disastroid.levels.Leveler;
-import com.aj.games.disastroid.levels.Leveler.ILevelListener;
+import com.aj.games.disastroid.obstacle.ObstaclePopulater;
 import com.aj.games.disastroid.ship.Ship;
 import com.aj.games.disastroid.time.TickerTimer;
 import com.aj.games.disastroid.time.TickerTimer.ITickerTimerListener;
 import com.aj.games.disastroid.time.TickerTimer.TickInterval;
 
-public class PlayPresenter implements ITickerTimerListener, ILevelListener {
+public class PlayPresenter implements ITickerTimerListener {
 
     private static final long PERIOD = 33L;
     private Activity activity;
@@ -21,18 +26,43 @@ public class PlayPresenter implements ITickerTimerListener, ILevelListener {
     private Ship ship;
     private PlayView view;
     private Leveler leveler;
+    private ObstaclePopulater obstaclePopulater;
 
     public PlayPresenter(Activity activity) {
 	this.activity = activity;
 	this.view = new PlayView(this, activity);
 	this.tickerTimer = new TickerTimer(PERIOD);
 
+	Toast.makeText(this.activity, "Loading...", Toast.LENGTH_LONG).show();
+	initializeAfterDelay();
+    }
+
+    private void initializeAfterDelay() {
+	Timer t = new Timer();
+	t.schedule(new TimerTask() {
+	    public void run() {
+		activity.runOnUiThread(new Runnable() {
+		    public void run() {
+			initialize();
+		    }
+		});
+	    }
+	}, 3000);
+    }
+
+    private void initialize() {
+	Display display = this.activity.getWindowManager().getDefaultDisplay();
+	Point size = new Point();
+	display.getSize(size);
+
+	this.obstaclePopulater = new ObstaclePopulater(new Rect(0, 0, size.x, size.y));
+
 	this.ship = new Ship();
-	this.leveler = new Leveler(this);
+	this.leveler = new Leveler(this.obstaclePopulater);
 
 	this.tickerTimer.registerNewListener(this);
 	this.tickerTimer.registerNewListener(this.leveler);
-
+	this.tickerTimer.registerNewListener(this.obstaclePopulater);
     }
 
     @Override
@@ -42,15 +72,6 @@ public class PlayPresenter implements ITickerTimerListener, ILevelListener {
 	activity.runOnUiThread(new Runnable() {
 	    public void run() {
 		view.update(ship);
-	    }
-	});
-    }
-
-    @Override
-    public void onLevelUp(final int level) {
-	activity.runOnUiThread(new Runnable() {
-	    public void run() {
-		Toast.makeText(activity, "Level up! " + level, Toast.LENGTH_SHORT).show();
 	    }
 	});
     }
