371c2b14497a9c1facad4f6ef8375c342fb35479
AndersonMAaron <AndersonMAaron@gmail.com>
2014-09-27 13:27:40 -0400

    Added update methods

--- Disastroid/src/com/aj/games/disastroid/ship/Ship.java
+++ Disastroid/src/com/aj/games/disastroid/ship/Ship.java
@@ -5,9 +5,10 @@ import java.util.Random;
 import android.util.Log;
 
 public class Ship {
-	private boolean isClockwise;
-	private int health;
-	private int angle; // Angle of the ship
+	private boolean isClockwise; // Direction the ship is rotating
+	private int health;			 // HP of the Ship
+	private int angle;			 // Angle of the ship
+	private int angularVelocity; // How many degrees the ship rotates on update
 	
 	private final int START_HEALTH = 100;
 	private final int ANGLE_MAX = 0;
@@ -25,24 +26,26 @@ public class Ship {
 		this.angle = angle;
 	}
 	
-	public void changeRotation() {
-		isClockwise = !isClockwise;
-	}
-	
-	public void updateAngle(int angle) {
-		if (!isAngleInRange(angle)) { 
-			Log.e(Ship.class.getName(), "Attempted to update to inappropriate angle " + angle);
-			return; 
-		}
-		this.angle = angle;
-	}
-	
-	public boolean isAngleInRange(int angle) {
-		return (0 < angle && angle < ANGLE_MAX);
+	/* UPDATES */
+	public void onUpdate() {
+		angle = isClockwise ? (angle - angularVelocity) : (angle + angularVelocity);
+		// Check for wrap-around (If the end of the ship we are tracking moves past 1st or 2nd quad)
+		if (angle < 0) { angle = ANGLE_MAX - angle; } 
+		else if (angle > ANGLE_MAX) { angle = angle - ANGLE_MAX; }
 	}
 	
 	public void takeHit(int hitAmount) {
 		health -= hitAmount;
 		if (health <= 0) { /*TODO game over */ }
 	}
+	
+	/* SETTERS */
+	public void changeRotation() {
+		isClockwise = !isClockwise;
+	}
+
+	public void setAngularVelocity(int angularVelocity) {
+		this.angularVelocity = angularVelocity;
+	}
+
 }
