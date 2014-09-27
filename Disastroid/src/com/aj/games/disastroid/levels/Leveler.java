5dbfdeda022897f15890593213c7bf05158658f2
gjinfo <gjinfo737@gmail.com>
2014-09-27 15:06:01 -0400

    toasts when leveling up

--- Disastroid/src/com/aj/games/disastroid/levels/Leveler.java
+++ Disastroid/src/com/aj/games/disastroid/levels/Leveler.java
@@ -11,6 +11,17 @@ public class Leveler implements ITickerTimerListener {
     private static final int SECONDS_BETWEEN_LEVEL_INCREMENT = 10;
     private int level = 1;
     private int playSeconds;
+    private ILevelListener levelListener;
+
+    public interface ILevelListener {
+
+	public void onLevelUp(int level);
+
+    }
+
+    public Leveler(ILevelListener levelListener) {
+	this.levelListener = levelListener;
+    }
 
     @Override
     public void onTimerTick(List<TickInterval> intervals, int tick, long period) {
@@ -29,6 +40,7 @@ public class Leveler implements ITickerTimerListener {
     private void levelUp() {
 	level++;
 	playSeconds = 0;
+	this.levelListener.onLevelUp(level);
     }
 
 }
--- Disastroid/src/com/aj/games/disastroid/play/PlayPresenter.java
+++ Disastroid/src/com/aj/games/disastroid/play/PlayPresenter.java
@@ -3,13 +3,16 @@ package com.aj.games.disastroid.play;
 import java.util.List;
 
 import android.app.Activity;
+import android.widget.Toast;
 
+import com.aj.games.disastroid.levels.Leveler;
+import com.aj.games.disastroid.levels.Leveler.ILevelListener;
 import com.aj.games.disastroid.ship.Ship;
 import com.aj.games.disastroid.time.TickerTimer;
 import com.aj.games.disastroid.time.TickerTimer.ITickerTimerListener;
 import com.aj.games.disastroid.time.TickerTimer.TickInterval;
 
-public class PlayPresenter implements ITickerTimerListener {
+public class PlayPresenter implements ITickerTimerListener, ILevelListener {
 
     private static final long PERIOD = 33L;
     private Activity activity;
@@ -17,14 +20,18 @@ public class PlayPresenter implements ITickerTimerListener {
     private TickerTimer tickerTimer;
     private Ship ship;
     private PlayView view;
+    private Leveler leveler;
 
     public PlayPresenter(Activity activity) {
 	this.activity = activity;
 	this.view = new PlayView(this, activity);
 	this.tickerTimer = new TickerTimer(PERIOD);
-	this.tickerTimer.registerNewListener(this);
 
 	this.ship = new Ship();
+	this.leveler = new Leveler(this);
+
+	this.tickerTimer.registerNewListener(this);
+	this.tickerTimer.registerNewListener(this.leveler);
 
     }
 
@@ -35,6 +42,15 @@ public class PlayPresenter implements ITickerTimerListener {
 	activity.runOnUiThread(new Runnable() {
 	    public void run() {
 		view.update(ship);
+	    }
+	});
+    }
+
+    @Override
+    public void onLevelUp(final int level) {
+	activity.runOnUiThread(new Runnable() {
+	    public void run() {
+		Toast.makeText(activity, "Level up! " + level, Toast.LENGTH_SHORT).show();
 	    }
 	});
     }
