82d7c26e83a80623c78121761dd6c5e3ee23fea9
AndersonMAaron <AndersonMAaron@gmail.com>
2014-09-27 18:22:09 -0400

    Difficulty now scales with level

--- Disastroid/src/com/aj/games/disastroid/levels/Leveler.java
+++ Disastroid/src/com/aj/games/disastroid/levels/Leveler.java
@@ -8,39 +8,39 @@ import com.aj.games.disastroid.time.TickerTimer.TickInterval;
 
 public class Leveler implements ITickerTimerListener {
 
-    private static final int SECONDS_BETWEEN_LEVEL_INCREMENT = 10;
-    private int level = 1;
-    private int playSeconds;
-    private ILevelListener levelListener;
+	private static final int SECONDS_BETWEEN_LEVEL_INCREMENT = 10;
+	private int level = 1;
+	private int playSeconds;
+	private ILevelListener levelListener;
 
-    public interface ILevelListener {
+	public interface ILevelListener {
 
-	public void onLevelUp(int level);
+		public void onLevelUp(int level);
 
-    }
-
-    public Leveler(ILevelListener levelListener) {
-	this.levelListener = levelListener;
-    }
-
-    @Override
-    public void onTimerTick(List<TickInterval> intervals, int tick, long period) {
-	if (TickerTimer.everySeconds(1, tick, period)) {
-	    playSeconds++;
-	    if (shouldLevelUp()) {
-		levelUp();
-	    }
 	}
-    }
 
-    private boolean shouldLevelUp() {
-	return playSeconds >= SECONDS_BETWEEN_LEVEL_INCREMENT;
-    }
+	public Leveler(ILevelListener levelListener) {
+		this.levelListener = levelListener;
+	}
 
-    private void levelUp() {
-	level++;
-	playSeconds = 0;
-	this.levelListener.onLevelUp(level);
-    }
+	@Override
+	public void onTimerTick(List<TickInterval> intervals, int tick, long period) {
+		if (TickerTimer.everySeconds(1, tick, period)) {
+			playSeconds++;
+			if (shouldLevelUp()) {
+				levelUp();
+			}
+		}
+	}
+
+	private boolean shouldLevelUp() {
+		return playSeconds >= SECONDS_BETWEEN_LEVEL_INCREMENT;
+	}
+
+	private void levelUp() {
+		level++;
+		playSeconds = 0;
+		this.levelListener.onLevelUp(level);
+	}
 
 }
--- Disastroid/src/com/aj/games/disastroid/obstacle/ObstaclePopulater.java
+++ Disastroid/src/com/aj/games/disastroid/obstacle/ObstaclePopulater.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.aj.games.disastroid.MainActivity;
 import com.aj.games.disastroid.levels.Leveler.ILevelListener;
 import com.aj.games.disastroid.time.TickerTimer;
 import com.aj.games.disastroid.time.TickerTimer.ITickerTimerListener;
@@ -13,100 +16,102 @@ import com.aj.games.disastroid.time.TickerTimer.TickInterval;
 
 public class ObstaclePopulater implements ITickerTimerListener, ILevelListener {
 
-    private List<Obstacle> obstacles = new ArrayList<Obstacle>();
-    private static final float SAFE_ANGLE_PADDING = 15;
-    private int level;
-    private Rect populationRect;
-    private float radius;
-    private SafeArea safeArea;
-    private float safeAreaChangePeriod = 10;
-    private float populatePeriod = .5f;
-    private float chanceOfPopulate = .5f;
+	private List<Obstacle> obstacles = new ArrayList<Obstacle>();
+	private static final float SAFE_ANGLE_PADDING = 15;
+	private int level;
+	private Rect populationRect;
+	private float radius;
+	private SafeArea safeArea;
+	private float safeAreaChangePeriod = 10;
+	private float populatePeriod = .5f;
+	private float chanceOfPopulate = .1f;
 
-    public ObstaclePopulater(Rect populationRect) {
-	this.populationRect = populationRect;
-	caculateRadius();
-	createSafeArea();
-    }
-
-    private void caculateRadius() {
-	int smallestDimmension = this.populationRect.width() > this.populationRect.height() ? this.populationRect.height() : this.populationRect
-		.width();
-	radius = (float) smallestDimmension / 4f;
-    }
-
-    private void createSafeArea() {
-	safeArea = new SafeArea(SAFE_ANGLE_PADDING);
-    }
-
-    @Override
-    public void onTimerTick(List<TickInterval> intervals, int tick, long period) {
-	for (Obstacle ob : obstacles) {
-	    ob.onUpdate();
+	public ObstaclePopulater(Rect populationRect) {
+		this.populationRect = populationRect;
+		caculateRadius();
+		createSafeArea();
 	}
 
-	if (TickerTimer.everySeconds(safeAreaChangePeriod, tick, period)) {
-	    createSafeArea();
+	private void caculateRadius() {
+		int smallestDimmension = this.populationRect.width() > this.populationRect
+				.height() ? this.populationRect.height() : this.populationRect
+				.width();
+		radius = (float) smallestDimmension / 4f;
 	}
 
-	if (TickerTimer.everySeconds(populatePeriod, tick, period)) {
-	    if (Math.random() < chanceOfPopulate) {
-		populate();
-	    }
-	    while (!cleanObstaclesList()) {
-	    }
-	}
-    }
-
-    private boolean cleanObstaclesList() {
-	if (obstacles == null)
-	    return true;
-	if (obstacles.size() == 0) {
-	    return true;
+	private void createSafeArea() {
+		safeArea = new SafeArea(SAFE_ANGLE_PADDING);
 	}
 
-	int removeIndex = -1;
-	for (int i = 0; i < obstacles.size(); i++) {
-	    Obstacle ob = obstacles.get(i);
-	    if (ob.hasPassed()) {
-		removeIndex = i;
-		break;
-	    }
-	}
-	if (removeIndex != -1) {
-	    obstacles.remove(removeIndex);
-	    return false;
-	} else {
-	    return true;
-	}
+	@Override
+	public void onTimerTick(List<TickInterval> intervals, int tick, long period) {
+		for (Obstacle ob : obstacles) {
+			ob.onUpdate();
+		}
 
-    }
+		if (TickerTimer.everySeconds(safeAreaChangePeriod, tick, period)) {
+			createSafeArea();
+		}
 
-    private void populate() {
-	obstacles.add(new Obstacle((int) radius, createRandomPointOnCircle()));
-    }
-
-    private Point createRandomPointOnCircle() {
-	Point point = new Point();
-	float angle = (float) (Math.random() * 360f);
-	while (this.safeArea.isInArea(angle)) {
-	    angle = (float) (Math.random() * 360f);
+		if (TickerTimer.everySeconds(populatePeriod, tick, period)) {
+			if (Math.random() < chanceOfPopulate) {
+				populate();
+			}
+			while (!cleanObstaclesList()) {
+			}
+		}
 	}
 
-	point.x = (int) (radius * Math.cos(angle)) + populationRect.centerX();
-	point.y = (int) (radius * Math.sin(angle)) + populationRect.centerY();
+	private boolean cleanObstaclesList() {
+		if (obstacles == null)
+			return true;
+		if (obstacles.size() == 0) {
+			return true;
+		}
 
-	return point;
-    }
+		int removeIndex = -1;
+		for (int i = 0; i < obstacles.size(); i++) {
+			Obstacle ob = obstacles.get(i);
+			if (ob.hasPassed()) {
+				removeIndex = i;
+				break;
+			}
+		}
+		if (removeIndex != -1) {
+			obstacles.remove(removeIndex);
+			return false;
+		} else {
+			return true;
+		}
 
-    @Override
-    public void onLevelUp(int level) {
-	this.level = level;
+	}
 
-    }
+	private void populate() {
+		obstacles.add(new Obstacle((int) radius, createRandomPointOnCircle()));
+	}
 
-    public List<Obstacle> getObstacles() {
-	return obstacles;
-    }
+	private Point createRandomPointOnCircle() {
+		Point point = new Point();
+		float angle = (float) (Math.random() * 360f);
+		while (this.safeArea.isInArea(angle)) {
+			angle = (float) (Math.random() * 360f);
+		}
+
+		point.x = (int) (radius * Math.cos(angle)) + populationRect.centerX();
+		point.y = (int) (radius * Math.sin(angle)) + populationRect.centerY();
+
+		return point;
+	}
+
+	@Override
+	public void onLevelUp(int level) {
+		this.level = level;
+		chanceOfPopulate = .1f * level;
+		Log.i("", "Levelled up to level " + level);
+	}
+
+	public List<Obstacle> getObstacles() {
+		return obstacles;
+	}
 
 }
